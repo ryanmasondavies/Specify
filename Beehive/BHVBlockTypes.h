d2c80a84fcc4c256e1a9a4c01badf89d16b66ce8
Ryan Davies <ryan@ryandavies.net>
2012-11-29 00:09:35 +0000

    Removed implementation block type.

--- Beehive/BHVBlockTypes.h
+++
@@ -1,9 +0,0 @@
-//
-//  BHVBlockTypes.h
-//  Beehive
-//
-//  Created by Ryan Davies on 21/11/2012.
-//  Copyright (c) 2012 Ryan Davies. All rights reserved.
-//
-
-typedef void(^BHVImplementationBlock)(void);
