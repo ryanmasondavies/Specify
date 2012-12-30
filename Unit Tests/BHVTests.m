122423aaa5b42dd839771c835a2a27a589f2799c
Ryan Davies <ryan@ryandavies.net>
2012-12-30 12:06:15 +0000

    Added blank project with unit tests.

---
+++ Unit Tests/BHVTests.m
@@ -0,0 +1,36 @@
+//
+//  BHVTests.m
+//  Unit Tests
+//
+//  Created by Ryan Davies on 30/12/2012.
+//  Copyright (c) 2012 Ryan Davies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BHVTests : SenTestCase
+
+@end
+
+@implementation BHVTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BehaveTests");
+}
+
+@end
