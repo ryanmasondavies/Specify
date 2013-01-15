6b60cfff85a5b5131f6c214ec05145852b083a9f
Ryan Davies <ryan@ryandavies.net>
2013-01-15 17:41:28 +0000

    Removed debug lines.

--- Integrated Tests/BHVBeforeEachSpec.m
+++ Integrated Tests/BHVBeforeEachSpec.m
@@ -11,23 +11,19 @@ SpecBegin(BHVBeforeEach)
 NSMutableString *order = [NSMutableString string];
 
 beforeEach(^{
-    NSLog(@"Before each 1");
     [order appendString:@"1"];
 });
 
 it(@"should have executed outer hook once", ^{
-    NSLog(@"Example 1");
     [[order should] beEqualTo:@"1"];
 });
 
 describe(@"in another context", ^{
     beforeEach(^{
-        NSLog(@"Before each 2");
         [order appendString:@"2"];
     });
     
     it(@"should have executed outer hook twice and inner hook once", ^{
-        NSLog(@"Example 2");
         [[order should] beEqualTo:@"112"];
     });
 });
